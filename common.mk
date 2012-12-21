587f7c7d9261ba9e3683d9f6c4daa7f8c4eb305d
Steve Kondik <shade@chemlab.org>
2012-12-20 21:32:01 -0800

    Update include paths
    Change-Id: I463eb0a6693ec5da41118f132d8dcdb0d11e4be8

--- common.mk
+++ common.mk
@@ -1,11 +1,11 @@
 #Common headers
-common_includes := hardware/qcom/display/libgralloc
-common_includes += hardware/qcom/display/liboverlay
-common_includes += hardware/qcom/display/libcopybit
-common_includes += hardware/qcom/display/libqdutils
-common_includes += hardware/qcom/display/libhwcomposer
-common_includes += hardware/qcom/display/libexternal
-common_includes += hardware/qcom/display/libqservice
+common_includes := hardware/qcom/display-caf/libgralloc
+common_includes += hardware/qcom/display-caf/liboverlay
+common_includes += hardware/qcom/display-caf/libcopybit
+common_includes += hardware/qcom/display-caf/libqdutils
+common_includes += hardware/qcom/display-caf/libhwcomposer
+common_includes += hardware/qcom/display-caf/libexternal
+common_includes += hardware/qcom/display-caf/libqservice
 
 ifeq ($(TARGET_USES_POST_PROCESSING),true)
     common_flags     += -DUSES_POST_PROCESSING
@@ -14,7 +14,7 @@ endif
 
 common_header_export_path := qcom/display
 
-#Common libraries external to display HAL
+#Common libraries external to display-caf HAL
 common_libs := liblog libutils libcutils libhardware
 
 ifeq ($(TARGET_USES_POST_PROCESSING),true)
