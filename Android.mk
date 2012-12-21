0484b4635e00bc191228c77d2a2bef605030e3eb
Steve Kondik <shade@chemlab.org>
2012-12-20 20:48:02 -0800

    Only build CAF display variant when requested.
    Change-Id: I273b9e16afa011445e185fdaf1e7cf2f029b077c

--- Android.mk
+++ Android.mk
@@ -1,3 +1,4 @@
+ifeq ($(TARGET_QCOM_DISPLAY_VARIANT),caf)
 display-hals := libgralloc libgenlock libcopybit
 display-hals += libhwcomposer liboverlay libqdutils libexternal libqservice
 ifneq ($(TARGET_PROVIDES_LIBLIGHT),true)
@@ -10,5 +11,6 @@ else
 ifneq ($(filter msm8960,$(TARGET_BOARD_PLATFORM)),)
     #This is for mako since it doesn't have the QCOM make functions
     include $(call all-named-subdir-makefiles,$(display-hals))
+endif
 endif
 endif
