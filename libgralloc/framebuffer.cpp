78b5abc867279d126c0a48e6650f72d48cbfff1b
Rudolf Tammekivi <rtammekivi@gmail.com>
2013-01-10 18:43:20 +0200

    libgralloc: Set alpha mode only if kernel allows.
    The alpha mode is not in the latest QCOM HAL, so we will provide a
    workaround for devices that need it, and still keep devices without
    this working.
    
    Change-Id: I0bf99cda698d9c78f0af7b8578a3fed21a9379a5

--- libgralloc/framebuffer.cpp
+++ libgralloc/framebuffer.cpp
@@ -245,6 +245,7 @@ int mapFrameBufferLocked(struct private_module_t* module)
     uint32_t line_length = (info.xres * info.bits_per_pixel / 8);
     info.yres_virtual = (size * numberOfBuffers) / line_length;
 
+#ifdef MSMFB_METADATA_SET
     struct msmfb_metadata metadata;
 
     metadata.op = metadata_op_base_blend;
@@ -253,6 +254,7 @@ int mapFrameBufferLocked(struct private_module_t* module)
     if(ioctl(fd, MSMFB_METADATA_SET, &metadata) == -1) {
         ALOGW("MSMFB_METADATA_SET failed to configure alpha mode");
     }
+#endif
 
     uint32_t flags = PAGE_FLIP;
 
