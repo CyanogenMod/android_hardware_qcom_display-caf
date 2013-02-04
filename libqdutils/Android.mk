a3b4db482cded5f70482b2f24e90eed7bcc4cf43
Evan McClain <aeroevan@gmail.com>
2013-02-03 19:37:01 -0500

    libqdutils: Use -Wno-attributes
    Change-Id: I25f19595ed29434938861a5462edc27e8d8ef717

--- libqdutils/Android.mk
+++ libqdutils/Android.mk
@@ -6,7 +6,7 @@ LOCAL_MODULE                  := libqdutils
 LOCAL_MODULE_TAGS             := optional
 LOCAL_SHARED_LIBRARIES        := $(common_libs)
 LOCAL_C_INCLUDES              := $(common_includes) $(kernel_includes)
-LOCAL_CFLAGS                  := $(common_flags) -DLOG_TAG=\"qdutils\"
+LOCAL_CFLAGS                  := $(common_flags) -Wno-attributes -DLOG_TAG=\"qdutils\"
 LOCAL_ADDITIONAL_DEPENDENCIES := $(common_deps)
 LOCAL_SRC_FILES               := profiler.cpp mdp_version.cpp \
                                  idle_invalidator.cpp \
