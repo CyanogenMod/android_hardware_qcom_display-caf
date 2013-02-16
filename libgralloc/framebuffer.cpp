6dfb7f10aa78b3e2c4176b381c4ac1165082efb2
Steve Kondik <shade@chemlab.org>
2013-02-16 02:28:56 -0800

    gralloc: Remove usage of METADATA_SET ioctl
     * No longer needed to configure base layer alpha
    
    Change-Id: I21ae663c530da9c848a3605a9f16bedbeea1502e

--- libgralloc/framebuffer.cpp
+++ libgralloc/framebuffer.cpp
@@ -123,7 +123,7 @@ int mapFrameBufferLocked(struct private_module_t* module)
     char name[64];
     char property[PROPERTY_VALUE_MAX];
 
-#if defined(MSMFB_METADATA_SET) || defined(MSMFB_METADATA_GET)
+#if defined(MSMFB_METADATA_GET)
     struct msmfb_metadata metadata;
 #endif
 
@@ -225,15 +225,6 @@ int mapFrameBufferLocked(struct private_module_t* module)
     //consider the included hole by 4k alignment
     uint32_t line_length = (info.xres * info.bits_per_pixel / 8);
     info.yres_virtual = (size * numberOfBuffers) / line_length;
-
-#ifdef MSMFB_METADATA_SET
-    metadata.op = metadata_op_base_blend;
-    metadata.flags = 0;
-    metadata.data.blend_cfg.is_premultiplied = 1;
-    if(ioctl(fd, MSMFB_METADATA_SET, &metadata) == -1) {
-        ALOGW("MSMFB_METADATA_SET failed to configure alpha mode");
-    }
-#endif
 
     uint32_t flags = PAGE_FLIP;
 
