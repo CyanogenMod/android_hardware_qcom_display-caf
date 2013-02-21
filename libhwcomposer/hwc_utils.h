802227bbfeccee499d74b0dbb978119e4b0fe1cb
Jeykumar Sankaran <jsanka@codeaurora.org>
2013-02-20 18:32:01 -0800

    display: Remove framebuffer HAL usage
    This patch removes the usage of the framebuffer HAL which is
    deprecated in JB MR1 onwards. The code is left for compatibility
    such as conformance tests but it is unused for normal display
    usage.
    
    Change-Id: If98133bdaa759cdc41d4503ff695b225ee43cb6f

--- libhwcomposer/hwc_utils.h
+++ libhwcomposer/hwc_utils.h
@@ -44,7 +44,6 @@
 
 //Fwrd decls
 struct hwc_context_t;
-struct framebuffer_device_t;
 
 namespace ovutils = overlay::utils;
 
@@ -264,8 +263,6 @@ struct vsync_state {
 struct hwc_context_t {
     hwc_composer_device_1_t device;
     const hwc_procs_t* proc;
-    //Framebuffer device
-    framebuffer_device_t *mFbDev;
 
     //CopyBit objects
     qhwc::CopyBit *mCopyBit[MAX_DISPLAYS];
