33d901ac80eb83d93be5d06ef346c7527f817218
Naseer Ahmed <naseer@codeaurora.org>
2013-03-18 11:46:24 -0400

    gralloc: Stride information fix
    The perform enum for the stride is defined in gralloc_priv so it
    is OK to keep the case outside.
    
    Change-Id: Id31556f76c2a2ae93a15a2e2dc4169b07360e4e6

--- libgralloc/mapper.cpp
+++ libgralloc/mapper.cpp
@@ -325,6 +325,7 @@ int gralloc_perform(struct gralloc_module_t const* module,
                 res = 0;
             }
             break;
+#endif
         case GRALLOC_MODULE_PERFORM_GET_STRIDE:
             {
                 int width   = va_arg(args, int);
@@ -333,7 +334,6 @@ int gralloc_perform(struct gralloc_module_t const* module,
                 *stride = AdrenoMemInfo::getInstance().getStride(width, format);
                 res = 0;
             } break;
-#endif
         default:
             break;
     }
