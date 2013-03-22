0be6eacbd72a4ff03a1eb0d64c98adaaa71e7e65
Saurabh Shah <saurshah@codeaurora.org>
2013-03-22 11:29:44 -0700

    hwc: Configure pipe for FB even if we exceed max layers
    Even if we exceed max supported layers, configure pipe for FB
    Because FB contents are always displayed using hwc
    
    Also some minor code fixes
    
    Change-Id: I740ab7e908c916373b1d87facd831e09098ce753

--- libhwcomposer/hwc_fbupdate.cpp
+++ libhwcomposer/hwc_fbupdate.cpp
@@ -1,6 +1,6 @@
 /*
  * Copyright (C) 2010 The Android Open Source Project
- * Copyright (C) 2012, The Linux Foundation. All rights reserved.
+ * Copyright (C) 2012-2013, The Linux Foundation. All rights reserved.
  *
  * Not a Contribution, Apache license notifications and license are
  * retained for attribution purposes only.
@@ -67,10 +67,6 @@ bool FBUpdateLowRes::configure(hwc_context_t *ctx,
     if (LIKELY(ctx->mOverlay)) {
         overlay::Overlay& ov = *(ctx->mOverlay);
         private_handle_t *hnd = (private_handle_t *)layer->handle;
-        if (!hnd) {
-            ALOGE("%s:NULL private handle for layer!", __FUNCTION__);
-            return false;
-        }
         ovutils::Whf info(hnd->width, hnd->height,
                 ovutils::getMdpFormat(hnd->format), hnd->size);
 
@@ -177,10 +173,6 @@ bool FBUpdateHighRes::configure(hwc_context_t *ctx,
     if (LIKELY(ctx->mOverlay)) {
         overlay::Overlay& ov = *(ctx->mOverlay);
         private_handle_t *hnd = (private_handle_t *)layer->handle;
-        if (!hnd) {
-            ALOGE("%s:NULL private handle for layer!", __FUNCTION__);
-            return false;
-        }
         ovutils::Whf info(hnd->width, hnd->height,
                 ovutils::getMdpFormat(hnd->format), hnd->size);
 
