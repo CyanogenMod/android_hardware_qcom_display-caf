aa3f91499a47c1d16bc99f7ef98a42a00ee47704
Steve Kondik <shade@chemlab.org>
2013-04-11 12:36:02 -0700

    display: Use actual kernel headers for all builds
    Change-Id: I1add22c0bc33942bc8f89dbb37333bf08d49bf72

--- common.mk
+++ common.mk
@@ -36,6 +36,8 @@ kernel_includes :=
 # Executed only on QCOM BSPs
 ifeq ($(TARGET_USES_QCOM_BSP),true)
     common_flags += -DQCOM_BSP
+endif
+ifeq ($(call is-vendor-board-platform,QCOM),true)
     common_deps += $(TARGET_OUT_INTERMEDIATES)/KERNEL_OBJ/usr
     kernel_includes += $(TARGET_OUT_INTERMEDIATES)/KERNEL_OBJ/usr/include
 endif
