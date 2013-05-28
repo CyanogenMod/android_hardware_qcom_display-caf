b80f3c0d89b74d4ece715ab6ad295959a1189dcb
Naseer Ahmed <naseer@codeaurora.org>
2013-05-28 16:37:23 -0400

    display: Remove libmm-abl dependency
    This lib no longer needs to be linked to any component in the
    display HAL.
    
    Change-Id: Id332d3cb22fc35e16f655d4c6127d6f1891330b0

--- common.mk
+++ common.mk
@@ -7,19 +7,10 @@ common_includes += hardware/qcom/display-caf/libhwcomposer
 common_includes += hardware/qcom/display-caf/libexternal
 common_includes += hardware/qcom/display-caf/libqservice
 
-ifeq ($(TARGET_USES_POST_PROCESSING),true)
-    common_flags     += -DUSES_POST_PROCESSING
-    common_includes += $(TARGET_OUT_HEADERS)/pp/inc
-endif
-
 common_header_export_path := qcom/display
 
 #Common libraries external to display-caf HAL
 common_libs := liblog libutils libcutils libhardware
-
-ifeq ($(TARGET_USES_POST_PROCESSING),true)
-    common_libs += libmm-abl
-endif
 
 #Common C flags
 common_flags := -DDEBUG_CALC_FPS -Wno-missing-field-initializers
