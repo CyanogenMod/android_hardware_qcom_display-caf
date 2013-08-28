d582b8fe32d09e9641241cb6eddf11a89b9a7450
Saurabh Shah <saurshah@codeaurora.org>
2013-08-28 15:13:52 -0700

    hwc: mdpcomp: Make all pipes available if a frame fails
    Make all pipes in overlay available, if programming a frame (list)
    fails. The failure could be due to commit failure, in which case the
    overlay itself will make pipes available.
    
    The failure can also be owing to lack of pipes, since we don't allow
    switching pipes across mixers any more. This is particularly a problem
    for split-panels. This fix addresses this latter case.
    
    Change-Id: Ieac7ae7d03e5b9a93c7245fa160c8933593c2e09

--- libhwcomposer/hwc_mdpcomp.cpp
+++ libhwcomposer/hwc_mdpcomp.cpp
@@ -865,6 +865,7 @@ int MDPComp::prepare(hwc_context_t *ctx, hwc_display_contents_1_t* list) {
                         mCurrentFrame.fbZ)) {
                 ALOGE("%s configure framebuffer failed", __func__);
                 reset(numLayers, list);
+                ctx->mOverlay->clear(mDpy);
                 return -1;
             } else { //Success
                 //Any change in composition types needs an FB refresh
@@ -884,6 +885,7 @@ int MDPComp::prepare(hwc_context_t *ctx, hwc_display_contents_1_t* list) {
         //Acquire and Program MDP pipes
         if(!programMDP(ctx, list)) {
             reset(numLayers, list);
+            ctx->mOverlay->clear(mDpy);
             return -1;
         } else { //Success
             //Any change in composition types needs an FB refresh
@@ -914,11 +916,13 @@ int MDPComp::prepare(hwc_context_t *ctx, hwc_display_contents_1_t* list) {
             if(!ctx->mFBUpdate[mDpy]->prepare(ctx, list, mCurrentFrame.fbZ)) {
                 ALOGE("%s configure framebuffer failed", __func__);
                 reset(numLayers, list);
+                ctx->mOverlay->clear(mDpy);
                 return -1;
             }
         }
         if(!programYUV(ctx, list)) {
             reset(numLayers, list);
+            ctx->mOverlay->clear(mDpy);
             return -1;
         }
     } else {
