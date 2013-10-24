ceb45d510db9006901070cf2ad7888ed0954b942
Michael Bestas <mikeioannina@gmail.com>
2013-10-24 08:22:33 +0300

    Use CM standard qcom naming
    Change-Id: I5b0edd9f9b8b7e7adbdf4dd399e3734afdb2aaa1

--- libcopybit/Android.mk
+++ libcopybit/Android.mk
@@ -34,7 +34,7 @@ ifeq ($(TARGET_USES_C2D_COMPOSITION),true)
     LOCAL_SRC_FILES := copybit_c2d.cpp software_converter.cpp
     include $(BUILD_SHARED_LIBRARY)
 else
-    ifneq ($(call is-chipset-in-board-platform,msm7630),true)
+    ifneq ($(call is-chipset-in-board-platform,msm7x30),true)
         ifeq ($(call is-board-platform-in-list,$(MSM7K_BOARD_PLATFORMS)),true)
             LOCAL_CFLAGS += -DCOPYBIT_MSM7K=1
             LOCAL_SRC_FILES := software_converter.cpp copybit.cpp
