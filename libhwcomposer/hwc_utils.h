1e3812655cf87a4abe149c241a32ee7271668e47
Ramkumar Radhakrishnan <ramkumar@codeaurora.org>
2013-12-04 15:15:42 -0800

    hwc: Calculate viewframe from layers display frame information.
    Calculate view frame of  each display from the displayframe of all
    the application layers.
    
    Change-Id: I9ba4076b7b4419bd3559417fad28b4e1de6acff3

--- libhwcomposer/hwc_utils.h
+++ libhwcomposer/hwc_utils.h
@@ -403,6 +403,7 @@ struct hwc_context_t {
     qhwc::CablProp mCablProp;
     overlay::utils::Whf mPrevWHF[HWC_NUM_DISPLAY_TYPES];
     qhwc::HwcDebug *mHwcDebug[HWC_NUM_DISPLAY_TYPES];
+    hwc_rect_t mViewFrame[HWC_NUM_DISPLAY_TYPES];
 
     // No animation on External display feature
     // Notifies hwcomposer about the device orientation before animation.
